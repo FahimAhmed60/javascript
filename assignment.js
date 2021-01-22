573843e547ddb7cc88c8076c19f70fa7bf63488e
Fahim Ahmed <rifatahmedfahim88@gmail.com>
2021-01-22 20:05:21 +0600

    3 completed

--- assignment.js
+++ assignment.js
@@ -1,44 +1,52 @@
 // kilometerToMeter
-function kilometerToMeter(kilo){
-    var meter = kilo/1000;
-    return meter;
+function kilometerToMeter(distanceInKilometer){
+    if( distanceInKilometer>0){
+        var distanceInMeter = distanceInKilometer*1000;
+        return distanceInMeter;
+    }
+    else{
+        console.log("Sorry invalid input!");
+    }
 }
-var result = kilometerToMeter(156);
-console.log(result);
-
+var kiloMeter = kilometerToMeter (5);
+console.log(kiloMeter);
 //Budget Calculator 
 
-function budgetCalculator(price){
-    var sum = 0;
-    for(var i = 0; i< price.length; i++){
-        var element = price[i];
-        sum = sum + element;
-    }
-    return sum;
+function budgetCalculator(pieceOfWatch,pieceOfMobile,pieceOfLaptop){
+    var priceOfWatch = 50;
+    var priceOfMobile = 100;
+    var priceOfLaptop = 500;
+    var totalNeed = pieceOfWatch*priceOfWatch + pieceOfMobile*priceOfMobile + pieceOfLaptop*priceOfLaptop;
+    return totalNeed;
 }
-var price = [50, 100, 500];
-var result = budgetCalculator(price);
-console.log(result);
-
+var budgetAmount = budgetCalculator (2,3,4);
+console.log(budgetAmount);
 
 // hotelCost
-
-function hotelCost(num){
-
+function hotelCost(perDay){
+    first10DaysCost = 100;
+    second10DaysCost = 80;
+    restOfDayCostings = 50;
+        if  (perDay>0 && perDay<=10){
+            var totalCost = perDay*first10DaysCost;
+        }
+        else if(perDay>20){
+            var daysExtra20 = perDay-20;
+            var totalCost = 10*first10DaysCost+10*second10DaysCost + daysExtra20*restOfDayCostings;
+        }
+        return totalCost;
 }
-
+var totalCost = hotelCost(30);
+console.log(totalCost);
 // megaFriend
-
 function megaFriend(arr) {
-    let longest = arr[0];
-    for (let i = 1; i < arr.length; i++) {
+    var longest = arr[0];
+    for (var i = 1; i < arr.length; i++) {
         if (arr[i].length > longest.length) {
             longest = arr[i];
         }
     }
     return longest;
 }
-
-let arr = ["Fahim Ahmed", "Ornob", "Shihab Khan", "Mubin"];
-
+var arr = ["Fahim Ahmed", "Ornob", "Shihab Khan", "Mubin"];
 console.log(megaFriend(arr));
